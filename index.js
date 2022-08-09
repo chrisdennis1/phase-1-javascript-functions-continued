25af7323cd4ee43a927a88925d424f0752a29c33
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-08-09 06:43:05 -0700

    commit tree 391fc52e53271b2cfbe0a962a4c4a5bb0b2467e3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = "go to the office") {
+    return(`This Monday, I will ${activity}.`)
+}
+
+mondayWork()
+mondayWork("work from home")
